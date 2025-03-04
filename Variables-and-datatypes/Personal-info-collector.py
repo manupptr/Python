41cc13a2238a9b28446f338e26d88b83c9c4d206
manu pptr <manupptr2000@gmail.com>
2025-03-04 22:24:36 +0530

    commit

---
+++ Variables-and-datatypes/Personal-info-collector.py
@@ -0,0 +1,11 @@
+name = input("Manu:")
+age = int(input("24")) #converting the string into integer
+height = float(input("1.75"))
+Is_student = input("yes").lower() == "yes"
+
+# printing the collected information below
+print("\n Personal Information summary")
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"Height: {height} meters")
+print(f"Student: {Is_student}")
