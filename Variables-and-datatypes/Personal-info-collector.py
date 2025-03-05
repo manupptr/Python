2eb81fc6638a08977963d615cad040f052c8f0ff
manu pptr <manupptr2000@gmail.com>
2025-03-05 11:26:25 +0530

    commit

--- Variables-and-datatypes/Personal-info-collector.py
+++ Variables-and-datatypes/Personal-info-collector.py
@@ -3,9 +3,15 @@ age = int(input("Age: ")) #converting the string into integer
 height = float(input("Height: "))
 is_student = input("Are You a Student? (yes/No): ").lower() == "yes"
 
-# printing the collected information below
+# printing and saving the collected information into a file
+info = f"""
 print("\n Personal Information summary")
 print(f"Name: {name}")
 print(f"Age: {age}")
 print(f"Height: {height} meters")
 print(f"Student: {is_student}")
+"""
+print(info)
+with open("manu_info.txt","w") as file:
+    file.written(info)
+    print("Information collected succesfully in 'manu_info.txt'.")
